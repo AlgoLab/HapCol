4ee1395b1c4aac7ddb311645ac8c9246b5ee6560
ddcatania <dd.catania@gmail.com>
2016-06-21 22:59:09 +0200

    Calcola i due alleli con maggior frequenza e secondo una certa soglia 'epsilon' classifica gli SNP in omozigote o eterozigote

--- src/input-bam/statistics_v2.py
+++ src/input-bam/statistics_v2.py
@@ -6,85 +6,11 @@ import logging
 import os
 import re
 import sys
+from collections import Counter
+import time
 
 import progressbar
 import pysam
-import time
-
-
-def generate_statistics(snp_map, alignment_file):
-    logging.debug('Started method generate_statistics')
-    start_time = time.time()
-
-    in_sam = pysam.AlignmentFile(alignment_file, 'rb')
-
-    snp_total_number = len(snp_map)
-
-    snp_count = 0
-    prgbar = progressbar.ProgressBar(maxval=snp_total_number).start()
-    count_first_allele = 0
-    count_second_allele = 0
-    count_other_allele = 0
-
-    read_count = 0
-    for snp_name, snp_values in snp_map.items():
-        snp_count += 1
-        prgbar.update(snp_count)
-        logging.debug('Loaded snp: {0}'.format(snp_name))
-        snp_chrom = snp_values[0]
-        snp_start = snp_values[1]
-        snp_end = snp_values[2]
-        first_observed = snp_values[3]
-        second_observed = snp_values[4]
-        logging.debug(
-            'Reading chromosome: {0}, Starting: {1} - Ending{2}'.format(snp_chrom, snp_start, snp_end))
-        fetch_aln = in_sam.fetch(snp_chrom, snp_start, snp_end)
-        tot_fetch_aln = in_sam.count(snp_chrom, snp_start, snp_end)
-        logging.info(
-            'Number of reads aligned with snp: {0}'.format(tot_fetch_aln))
-
-        if tot_fetch_aln == 0 or tot_fetch_aln < 10:
-            logging.debug('SNP without at least ten items will be discarded.')
-            continue
-
-        for read in fetch_aln:
-            read_count += 1
-            read_name = read.query_name
-            read_start = read.reference_start
-            read_end = read.reference_end
-            read_sequence = read.query_sequence
-            logging.debug('Loaded read: {0}'.format(read_name))
-
-            if snp_start >= read_start and snp_end <= read_end:
-                logging.debug('In the read: {0}, in the SNP (first): {1}, in the SNP (second): {2}'.format(
-                    read_sequence[snp_start:snp_end], first_observed, second_observed))
-                if read_sequence[snp_start:snp_end] == first_observed:
-                    count_first_allele += 1
-                elif read_sequence[snp_start:snp_end] == second_observed:
-                    count_second_allele += 1
-                elif not read_sequence[snp_start:snp_end].strip():
-                    logging.debug('No valid read found.')
-                else:
-                    count_other_allele += 1
-    prgbar.finish()
-
-    logging.info('Total number of SNPs: {0}'.format(snp_total_number))
-
-    logging.info('Total number of reads: {0}'.format(read_count))
-
-    logging.info('Total number of read aligned with the first allele: {0} ({1}%)'.format(
-        count_first_allele, (float(count_first_allele) / read_count) * 100))
-
-    logging.info('Total number of read aligned with the second allele: {0} ({1}%)'.format(
-        count_second_allele, (float(count_second_allele) / read_count) * 100))
-
-    logging.info('Total number of read aligned with other allele: {0} ({1}%)'.format(
-        count_other_allele, (float(count_other_allele) / read_count) * 100))
-
-    logging.debug('Finished method generate_statistics in {0} seconds'.format(
-        time.time() - start_time))
-    logging.info('Generation of statistics: Completed')
-    in_sam.close()
 
 
 def main():
@@ -110,6 +36,10 @@ def main():
                         action='store_true',
                         dest='skip_first_line',
                         help='set this flag if the first line IS NOT the header')
+    parser.add_argument('-epsilon',
+                        action='store',
+                        dest='threshold',
+                        help='set the threshold for Homozygous or Heterozygous. Default: 80')
     parser.add_argument('-file-delimiter',
                         action='store',
                         dest='file_delim',
@@ -147,6 +77,10 @@ def main():
     else:
         ann_file_delimiter = args.file_delimiter
 
+    threshold = 80
+    if args.threshold is not None:
+        threshold = int(args.threshold)
+
     logging.info('#### STEP 1 - Loading SNPs ####')
 
     snp_map = reading_snp(args.annotationFile,
@@ -154,20 +88,12 @@ def main():
 
     logging.info('#### STEP 2 - Generating Statistics ####')
 
-    generate_statistics(snp_map, in_bam_file)
+    classify_snp(in_bam_file, snp_map, threshold, out_dir)
 
     logging.info('Program Completed')
 
 
 # utility
-
-def check_observed(observed_couple):
-    # observed are in the form A/T, T/-, -/C
-    regexpr_snp = re.compile(r'^(\-|\w+)\/(\-|\w+)')
-    couple = regexpr_snp.search(observed_couple)
-    return couple.group(1), couple.group(2)
-
-
 def prepare_loggers(log_level):
     logging.basicConfig(level=log_level,
                         format='%(levelname)-8s [%(asctime)s]  %(message)s',
@@ -192,28 +118,126 @@ def reading_snp(annotation_file, file_delimiter, flag_first_line):
     snp_header_index_start = header.index('chromStart')
     snp_header_index_stop = header.index('chromEnd')
     snp_header_index_name = header.index('name')
-    snp_header_observed = header.index('observed')
     snp_map = {}
     for line in ann_file:
+
         splitted_line = line.split(file_delimiter)
 
         snp_chrom = splitted_line[snp_header_index_chrom].split('chr', 1)[1]
         snp_start = int(splitted_line[snp_header_index_start])
         snp_stop = int(splitted_line[snp_header_index_stop])
         snp_name = splitted_line[snp_header_index_name]
-        first_observed, second_observed = check_observed(
-            splitted_line[snp_header_observed])
-        logging.debug('{0},{1},{2},{3}'.format(
-            snp_chrom, snp_start, snp_stop, snp_name))
-        snp_map[snp_name] = [snp_chrom, snp_start, snp_stop,
-                             first_observed, second_observed]
+
+        snp_map[snp_name] = [snp_chrom, snp_start, snp_stop]
 
     ann_file.close()
 
     logging.debug('Finished method reading_snp in: {0} seconds'.format(
         time.time() - start_time))
-
     return snp_map
+
+
+def classify_snp(alignment_file, snp_map, threshold, out_dir):
+    logging.debug('Started method classifySNP')
+    start_time = time.time()
+    count_homo = 0
+    count_hete = 0
+    snp_total_number = len(snp_map)
+    statistics_file = open(out_dir + 'statistics2.txt', 'w')
+
+    in_sam = pysam.AlignmentFile(alignment_file, 'rb')
+    prgbar = progressbar.ProgressBar(maxval=snp_total_number).start()
+    cnt = 0
+
+    for snp_name, snp_values in snp_map.items():
+        statistics_file.write('Reading SNP: {0}\n'.format(snp_name))
+        cnt += 1
+        prgbar.update(cnt)
+        snp_chrom = snp_values[0]
+        snp_start = snp_values[1]
+        snp_end = snp_values[2]
+
+        logging.debug(
+            'SNP start: {0} - SNP end:{1}'.format(snp_start, snp_end))
+
+        fetch_aln = in_sam.fetch(snp_chrom, snp_start, snp_end)
+        count_aln = in_sam.count(snp_chrom, snp_start, snp_end)
+
+        alleles = Counter()
+
+        for read in fetch_aln:
+            read_name = read.query_name
+            logging.debug('Reading read: {0}'.format(read_name))
+            read_sequence = read.query_sequence
+            read_base = read_sequence[snp_start:snp_end]
+            if not read_base:
+                logging.debug('Empty read - indel')
+                continue
+            logging.debug('Read base: {0}'.format(read_base))
+            alleles[read_base] += 1
+
+        logging.debug(alleles)
+        statistics_file.write('Allele counter for the SNP:\n')
+        for key, value in alleles.items():
+            statistics_file.write('{0} : {1}\n'.format(key, value))
+
+        if alleles:
+            first_allele = alleles.most_common(2)[0]
+            logging.debug(first_allele)
+            second_allele = alleles.most_common(2)[1]
+            logging.debug(second_allele)
+
+            if (first_allele[1] / count_aln) * 100 >= threshold:
+                statistics_file.write('Snp is Homozygous\n')
+                logging.debug('The snp {0} is: Homozygous'.format(snp_name))
+                count_homo += 1
+            else:
+                statistics_file.write('Snp is Heterozygous\n')
+                logging.debug('The snp {0} is: Heterozygous'.format(snp_name))
+                count_hete += 1
+
+        logging.debug(
+            'Number of read aligned on SNP: {0}'.format(count_aln))
+        statistics_file.write(
+            'Number of read aligned on SNP: {0}\n'.format(count_aln))
+
+        out = {}
+        if count_aln > 0:
+            for key in alleles:
+                out[key] = (float(alleles[key]) / count_aln) * 100
+
+        logging.debug(out)
+        statistics_file.write('Base Frequences for the SNP:\n')
+        for key, value in out.items():
+            statistics_file.write('{0} : {1}\n'.format(key, value))
+
+    prgbar.finish()
+    in_sam.close()
+
+    logging.debug(
+        'Total number of SNPs: {0}'.format(snp_total_number))
+
+    statistics_file.write(
+        'Total number of SNPs: {0}\n'.format(snp_total_number))
+
+    logging.debug(
+        'Number of SNPs: Homozygous {0} ({1}%)'.format(
+            count_homo, ((float(count_homo) / snp_total_number) * 100)))
+
+    statistics_file.write('Number of SNPs: Homozygous {0} ({1}%)\n'.format(
+        count_homo, ((float(count_homo) / snp_total_number) * 100)))
+
+    logging.debug(
+        'Number of SNPs: Heterozygous {0} ({1}%)'.format(
+            count_hete, ((float(count_hete) / snp_total_number) * 100)))
+
+    statistics_file.write('Number of SNPs: Heterozygous {0} ({1}%)\n'.format(
+        count_hete, ((float(count_hete) / snp_total_number) * 100)))
+
+    logging.debug('Finished method classifySNP in: {0} seconds'.format(
+        time.time() - start_time))
+
+    statistics_file.close()
 
 
 if __name__ == '__main__':
