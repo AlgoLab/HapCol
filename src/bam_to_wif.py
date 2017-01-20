fb56e960f637eebc5d7c163b1d4adcfba6ddef4c
Danilo Catania <d.catania@reply.it>
2017-01-20 14:54:31 +0100

    bam_to_wif: now chromosome can be selected as parameter. Minor fixes on wif write.

--- src/bam_to_wif.py
+++ src/bam_to_wif.py
@@ -17,16 +17,26 @@ def file_len(fname):
     return i + 1
 
 
-def load_snps(alignment_file, variant_file, file_delimiter):
+def mean(list_of_quals):
+    """
+    Naive function for determining 'read quality'
+    :param list_of_quals: list of numeric quality values
+    :return: mean of the quality values
+    """
+    return float(sum(list_of_quals)) / len(list_of_quals)
+
+
+def load_snps(alignment_file, variant_file, file_delimiter, chromosome, base_q=None, read_q=None):
     in_sam = pysam.AlignmentFile(alignment_file, 'rb')
 
     total_read_number = in_sam.count()
     logging.info('Number of fetched reads: {}'.format(total_read_number))
 
     out_map = {}
-
     total_variant_number = file_len(variant_file)
     var_file = open(variant_file)
+
+    valid = ['A', 'T', 'C', 'G']
 
     for line in tqdm(var_file, total=total_variant_number, desc="Variant counter", miniters=1):
 
@@ -35,11 +45,14 @@ def load_snps(alignment_file, variant_file, file_delimiter):
         snp_ref = splitted[1]
         snp_alt = splitted[2]
 
-        for read in in_sam.fetch("chr1", snp_position, snp_position + 1):
+        for read in in_sam.fetch(chromosome, snp_position, snp_position + 1):
+
+            if read_q and mean(read.query_qualities) < read_q:
+                # read quality threshold
+                continue
+
             if read.qname not in out_map:
                 out_map[read.qname] = {'snp_list': [], 'mq': 0}
-
-            valid = ['A', 'T', 'C', 'G']
 
             try:
                 base_value = read.seq[snp_position - read.reference_start]
@@ -58,9 +71,13 @@ def load_snps(alignment_file, variant_file, file_delimiter):
 
             try:
                 base_quality = read.query_qualities[snp_position - read.reference_start]
+                if base_q and base_quality < base_q:
+                    # base quality threshold
+                    continue
             except IndexError:
                 continue
 
+            logging.debug("Found Snp: {} {} {} {}".format(snp_position, base_value, allele, base_quality))
             out_map[read.qname]['snp_list'].append((snp_position, base_value, allele, base_quality))
             out_map[read.qname]['mq'] = read.mapping_quality
 
@@ -76,7 +93,7 @@ def write_wif(read_snp_map, out_dir):
         snp_list = snp_dict['snp_list']
         mq = snp_dict['mq']
 
-        if len(snp_list) == 0:  # No SNP found for this read
+        if len(snp_list) == 0:  # No SNP found for this read or read empty
             continue
 
         output_file.write("{}".format(read_name))
@@ -86,7 +103,7 @@ def write_wif(read_snp_map, out_dir):
             a = snp[2]  # allele
             q = snp[3]  # base quality
             output_file.write(" : {} {} {} {}".format(p, b, a, q))
-            output_file.write(" # {} {} NA\n".format(len(snp_list), mq))
+        output_file.write(" # {} {} NA\n".format(len(snp_list), mq))
 
 
 def calculate_coverage(read_snp_map):
@@ -96,7 +113,7 @@ def calculate_coverage(read_snp_map):
         snp_list = snp_dict['snp_list']
         cov += len(snp_list)
 
-    avg = cov / read_number
+    avg = (cov / read_number) * 100
     logging.info("Average Coverage for {}: {}%".format(read_number, avg))
 
 
@@ -110,10 +127,14 @@ def main():
                         help='Output (root) directory. Default: current directory')
     parser.add_argument('-file-delimiter', action='store', dest='fileDelim',
                         help='Set the file delimiter for the variant file. Default: \\t')
-    parser.add_argument('-c', action='store_true', dest='coverageFlag', help="If set calculate reads coverage and "
-                                                                             "return to stdout", default=False)
-    parser.add_argument('--skip-write', '-sw', action='store_true', dest='skipWrite',
-                        help='When set, no files will be written', default=False)
+    parser.add_argument('-c', action='store_true', dest='coverageFlag',
+                        help="If set calculate reads coverage and return to stdout", default=False)
+    parser.add_argument('--chromosome', '-chr', action='store', dest='chromosome',
+                        help='Chromosome to analyze', default='chr1')
+    parser.add_argument('--base-quality', '-bq', action='store', dest='baseQuality',
+                        help='Set the minimum quality for base', type=int)
+    parser.add_argument('--read-quality', '-rq', action='store', dest='readQuality',
+                        help='Set the minimum quality for read', type=int)
     parser.add_argument('-v', help='increase output verbosity', action='count')
 
     args = parser.parse_args()
@@ -147,15 +168,19 @@ def main():
 
     logging.info('#### Calculating SNPs info ####')
 
-    snps_map_info = load_snps(args.fileBAM, args.variantFile, ann_file_delimiter)
+    chromosome = 'chr1'
+    if args.chromosome:
+        chromosome = args.chromosome
+
+    snps_map_info = load_snps(args.fileBAM, args.variantFile, ann_file_delimiter, chromosome, args.baseQuality,
+                              args.readQuality)
 
     if args.coverageFlag:
         logging.info('#### Calculating coverage ####')
         calculate_coverage(snps_map_info)
 
-    if not args.skipWrite:
-        logging.info('#### Writing Wif ####')
-        write_wif(snps_map_info, out_dir)
+    logging.info('#### Writing Wif ####')
+    write_wif(snps_map_info, out_dir)
 
     logging.info('BamToWif: Program Completed')
 
